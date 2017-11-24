fdf35e5c28bda088a96618ebaaa7d2da21697451
Daniel <daniwhazzup@gmail.com>
2017-11-24 16:00:22 +0100

    added mittente

---
+++ mittente/js/mqtt_parameters.js
@@ -0,0 +1,14 @@
+// parameters MQTT broker
+var host = 'm23.cloudmqtt.com';
+var user = 'znkvsald';
+var psw = 'hIbJnld6vKon';
+var port = 35463;
+var canale = 'FAD_San_Paolo';
+var clientId = null;
+
+// costanti
+var client = null;
+
+// testi
+const MESSAGGIO_CONNESSIONE_AVVENUTA = 'Connessione avvenuta con successo';
+const MESSAGGIO_CONNESSIONE_FALLITA = 'Connessione fallita';
