ad369981b6c13bb7b056e3ff6aac71c02cfd7597
Daniel <daniwhazzup@gmail.com>
2017-11-24 16:25:25 +0100

    added graffle and obscured js

--- mittente/js/mqtt_parameters.js
+++ mittente/js/mqtt_parameters.js
@@ -1,9 +1,9 @@
 // parameters MQTT broker
-var host = 'm23.cloudmqtt.com';
-var user = 'znkvsald';
-var psw = 'hIbJnld6vKon';
-var port = 35463;
-var canale = 'FAD_San_Paolo';
+var host = '';
+var user = '';
+var psw = '';
+var port = ;
+var canale = '';
 var clientId = null;
 
 // costanti
